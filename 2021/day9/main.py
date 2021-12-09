609fc4ad32a6c1606e03d66f4e3e4bf9068ccf04
Kyle Peront <kaperont@gmail.com>
2021-12-09 14:09:04 -0500

    Day 9

--- 2021/day9/main.py
+++ 2021/day9/main.py
@@ -1,76 +1,83 @@
 import numpy as np
 
-class vertex:
-    value = 10000
-    lowest_neighbor = 10000
-    found = False
+def read_input(file = 'input.txt'):
+    with open(file) as f:
+            data = f.readlines()
+    input_matrix = [line.strip('\n')  for line in data]
+    output_matrix = []
+    for line in input_matrix:
+        out_line = [int(c) for c in line] 
+        output_matrix.append(out_line)
+    m = np.array(output_matrix)
 
-    def __init__(self, value):
-        self.value = value
-        self.lowest_neighbor = 10000
+    # brace the input
+    new_column = np.ones((len(m), 1), dtype=int)*np.inf
+    out = np.block([new_column, m, new_column])
+    new_row = np.ones((1, len(out[0])), dtype=int)*np.inf
+    out = np.vstack([new_row, out, new_row])
+    return out
 
-    def update_lowest_neighbor(self, lowpoint):
-        self.lowest_neighbor = lowpoint
+def part1(b):
+    # start at (1,1)
+    is_min_list = []
+    position_list = []
+    for row in range(1,len(b)-1):
+        for col in range(1,len(b[0])-1):
+            center = b[row][col]
+            up = b[row-1][col]
+            down = b[row+1][col] 
+            right = b[row][col+1]
+            left = b[row][col-1]
+            area = [up, down, right, left]
+            if (center < min(area)):
+                is_min_list.append(int(center))
+                position_list.append((row, col))
 
+    risk_level = np.add(is_min_list, 1).tolist()
+    return sum(risk_level), position_list
 
-input = open("input.txt", "r").readlines()
+def test():
+    input= np.array([[2,1,9,9,9,4,3,2,1,0],
+                    [3,9,8,7,8,9,4,9,2,1],
+                    [9,8,5,6,7,8,9,8,9,2],
+                    [8,7,6,7,8,9,6,7,8,9],
+                    [9,8,9,9,9,6,5,6,7,8]])
+    m = np.array(input)
 
-graph = []
+    # brace the input
+    new_column = np.ones((len(m), 1), dtype=int)*np.inf
+    out = np.block([m, new_column])
+    out = np.block([new_column, out])
+    new_row = np.ones((1, len(m[0])+2), dtype=int)*np.inf
+    out = np.vstack([out, new_row])
+    out = np.vstack([new_row, out])
 
-for line in input:
-    l = list(line)
-    l.pop(-1)
-    row = []
-    for num in l:
-        v = vertex(int(num))
-        row.append(v)
-    graph.append(row)
+    print('Result for part 1: ',part1(out))
+    print('Result for part 2: ',part2(out))
 
-#print(graph)
+def part2(b):
+    return np.prod(sorted([len(x) for x in [bfs(x[0], x[1], b) for x in part1(b)[1]]])[-3:])    
     
-i = 0
-for row in graph:
-    j = 0
-    for col in row:
-        if i==0 and j==0:
 
-            if col.value < graph[i][j+1].value and col.value < graph[i+1][j].value:
-                graph[i][j].lowest_neighbor = col.value
-            elif graph[i][j+1].value < graph[i+1][j].value:
-                graph[i][j].lowest_neighbor = graph[i][j+1].value
-            elif graph[i+1][j].value < graph[i][j+1].value:
-                graph[i][j].lowest_neighbor = graph[i+1][j].value
-            elif graph[i][j+1].value == graph[i+1][j].value:
-                graph[i][j].lowest_neighbor = graph[i][j+1].value
+def bfs(x:int,y:int, input_matrix:np.ndarray):
+    start = [(x, y)]
+    queue = start
+    visited = set()
+    while(queue):
+        (curr_x, curr_y) = start.pop(0)
+        if ((curr_x, curr_y) in visited or input_matrix[curr_x][curr_y] >= 9):
+            continue
+        visited.add((curr_x, curr_y))
+        # add elements in order up, down, left, right
+        queue.append((curr_x, curr_y-1))
+        queue.append((curr_x, curr_y+1))
+        queue.append((curr_x-1, curr_y))
+        queue.append((curr_x+1, curr_y))
+    return visited
 
-            print(graph[i][j].value, graph[i][j].lowest_neighbor)
-        
-        elif i==0 and j==99:
-
-            if col.value < graph[i][j-1].value and col.value < graph[i+1][j].value:
-                graph[i][j].lowest_neighbor = col.value
-            elif graph[i][j-1].value < graph[i+1][j].value:
-                graph[i][j].lowest_neighbor = graph[i][j-1].value
-            elif graph[i+1][j].value < graph[i][j-1].value:
-                graph[i][j].lowest_neighbor = graph[i+1][j].value
-            elif graph[i][j-1].value == graph[i+1][j].value:
-                graph[i][j].lowest_neighbor = graph[i][j-1].value
-
-            print(graph[i][j].value, graph[i][j].lowest_neighbor)
-
-        elif i==0 and j!=0:
-
-            if col.value < graph[i][j+1].value and col.value < graph[i][j-1].value and col.value < graph[i+1][j].value:
-                graph[i][j].lowest_neighbor = col.value
-            elif graph[i][j+1].value < graph[i+1][j].value and graph[i][j+1].value < graph[i][j-1].value:
-                graph[i][j].lowest_neighbor = graph[i][j+1].value
-            elif graph[i][j-1].value < graph[i+1][j].value and graph[i][j-1].value < graph[i][j+1].value:
-                graph[i][j].lowest_neighbor = graph[i][j-1].value
-            elif graph[i+1][j].value < graph[i][j+1].value and graph[i+1][j].value < graph[i][j-1].value:
-                graph[i][j].lowest_neighbor = graph[i+1][j].value
-            elif graph[i][j+1].value == graph[i+1][j].value and graph[i][j+1].value == graph[i][j-1].value:
-                graph[i][j].lowest_neighbor = graph[i][j+1].value
-
-            print(graph[i][j].value, graph[i][j].lowest_neighbor)
-        j += 1
-    i += 1
+if __name__ == '__main__':
+    input_matrix = read_input()
+    #test()
+    p1,_ = part1(input_matrix)
+    print('Result for part 1: ', p1)
+    print('Result for part 2: ',part2(input_matrix))
